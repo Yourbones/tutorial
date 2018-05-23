54ec74e02e1e15ea56caa43f230ccc37b75db396
Yourbones <xhx@taipuauto.cn>
2018-05-23 15:35:32 +0800

    修改model

--- blog/admin.py
+++ blog/admin.py
@@ -2,9 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Article
+from .models import Article, Category, Tag
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'pub_date')
+    list_display = ('title', 'click_nums', 'category', 'crate_time', 'modify_time')  # 博客相关字段展示
 
 admin.site.register(Article,ArticleAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -3,21 +3,52 @@ from __future__ import unicode_literals
 
 from django.db import models
 from DjangoUeditor.models import UEditorField
+from django.utils import timezone
+
+class Category(models.Model):
+    """
+    文章分类
+    """
+    name = models.CharField(verbose_name='文章类别', max_length=20)
+
+    class Meta:
+        verbose_name = '文章类别'
+        verbose_name_plural = verbose_name
+    def __unicode__(self):
+        return self.name
+
+class Tag(models.Model):
+    """
+    文章标签
+    """
+    name = models.CharField(verbose_name='文章标签', max_length=20)
+
+    class Meta:
+        verbose_name = '文章标签'
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return self.name
 
 class Article(models.Model):
-    title = models.CharField(u"博客标题", max_length = 100)
-    tag = models.CharField(u"博客标签", max_length= 50,blank = True)
-    pub_date = models.DateTimeField(u"发布日期", auto_now_add = True,editable = True)
-    update_time = models.DateTimeField(u"更新日期", auto_now = True, null=True)
-    content = UEditorField(u"文章正文", height=300, width=770, default=u'', blank=True,imagePath="uploads/blog/images/",
+    """
+    博客
+    """
+    title = models.CharField(verbose_name="博客标题", max_length = 100)
+    tag = models.ManyToManyField(Tag, verbose_name="文章标签")
+    category = models.ForeignKey(Category, verbose_name="文章类别")
+    create_time = models.DateTimeField(verbose_name="创建时间", auto_now_add = True,editable=True)
+    modify_time = models.DateTiemField(verbose_name="修改时间", auto_now=True)
+    click_nums = models.IntegerField(verbose_name="点击量", default=0)
+    content = UEditorField(verbose_name="文章正文", height=300, width=770, default=u'', blank=True,imagePath="uploads/blog/images/",
                            toolbars='besttome',filePath='uploads/blog/files/')
 
     def __unicode__(self):
         return self.title
 
     class Meta:
-        ordering = ['-pub_date']
-        verbose_name = "文章"
-        verbose_name_plural = "文章"
+        ordering = ['-create_time']
+        verbose_name = "我的博客"
+        verbose_name_plural = verbose_name
 
 
