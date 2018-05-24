ae2bc3083d30285b051b3051e71fb4e4a323f481
Yourbones <xhx@taipuauto.cn>
2018-05-24 15:27:00 +0800

    修改model字段

--- blog/admin.py
+++ blog/admin.py
@@ -4,9 +4,11 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import Article, Category, Tag
 
+@admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'click_nums', 'category', 'crate_time', 'modify_time')  # 博客相关字段展示
+    list_display = ('title', 'click_nums', 'category', 'create_time', 'modify_time')  # 博客相关字段展示
+    list_per_page = 10                      # 设置每页显示多少条记录，默认是100条
+    list_editable = ['category']            # 设置默认可编辑字段
 
-admin.site.register(Article,ArticleAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,7 @@ class Category(models.Model):
     class Meta:
         verbose_name = '文章类别'
         verbose_name_plural = verbose_name
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 class Tag(models.Model):
@@ -27,7 +27,7 @@ class Tag(models.Model):
         verbose_name = '文章标签'
         verbose_name_plural = verbose_name
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 class Article(models.Model):
@@ -38,12 +38,12 @@ class Article(models.Model):
     tag = models.ManyToManyField(Tag, verbose_name="文章标签")
     category = models.ForeignKey(Category, verbose_name="文章类别")
     create_time = models.DateTimeField(verbose_name="创建时间", auto_now_add = True,editable=True)
-    modify_time = models.DateTiemField(verbose_name="修改时间", auto_now=True)
+    modify_time = models.DateTimeField(verbose_name="修改时间", auto_now=True)
     click_nums = models.IntegerField(verbose_name="点击量", default=0)
     content = UEditorField(verbose_name="文章正文", height=300, width=770, default=u'', blank=True,imagePath="uploads/blog/images/",
                            toolbars='besttome',filePath='uploads/blog/files/')
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
 
     class Meta:
